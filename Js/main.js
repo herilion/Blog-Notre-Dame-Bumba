283032987b56cfbfbef9e8df5df0dd726e5b43f1
herilion <heritierlionge@yahoo.com>
2023-07-26 10:03:41 +0200

    update

--- Js/main.js
+++ Js/main.js
@@ -3,15 +3,16 @@ $(document).ready(function() {
     $('.filter-item').click(function(){
         const value=$(this).attr('data-filter')
         if(value=='all'){
-            $('.post-box').show('1000');
+            $('.post-box')
+            .show('1000');
         }
         else{
             $('.post-box')
                 .not('.'+ value)
-                .hide('1000')
+                .hide('1000');
             $('.post-box')
                 .not('.'+ value)
-                .hide('1000')
+                .hide('1000');
         }
     });
     
