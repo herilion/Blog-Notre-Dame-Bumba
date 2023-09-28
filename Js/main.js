f95c95b14a342e3c161667212628bffbf724821e
herilion <heritierlionge@yahoo.com>
2023-09-28 16:44:36 +0200

    update

--- Js/main.js
+++ Js/main.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
     });
     // Header Background change on scroll
     let header=document.querySelector('header');
-
+// 
     window.addEventListener('scroll',()=>{
         header.classList.toggle('shadow',window.scrollY>0);
     })
